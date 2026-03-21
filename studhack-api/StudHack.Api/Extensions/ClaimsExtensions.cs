91a754cbec572750bc8c32b596244e8e3f6ca6a3
SergeiKrivko <121343672+SergeiKrivko@users.noreply.github.com>
2026-03-21 11:24:59 +0300

    Backend authentication (#5)

---
+++ studhack-api/StudHack.Api/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace StudHack.Api.Extensions;
+
+public static class ClaimsExtensions
+{
+    public static Guid GetUserId(this ClaimsPrincipal principal)
+    {
+        return Guid.Parse(principal.FindFirst("UserId")?.Value ??
+                          throw new Exception("'UserId' claim was not found"));
+    }
+}
