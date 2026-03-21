bca55db459d8e9a9a9ed357382fddf114f1dc796
SergeiKrivko <sergik.krivko@gmail.com>
2026-03-21 11:03:14 +0300

    authentication

--- studhack-api/StudHack.Api/Program.cs
+++ studhack-api/StudHack.Api/Program.cs
@@ -1,8 +1,35 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+    {
+        options.MetadataAddress =
+            $"{builder.Configuration["Auth.Issuer"]}/.well-known/openid-configuration";
+        options.Authority = builder.Configuration["Auth.ApiUrl"];
+        options.RequireHttpsMetadata = false;
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["Auth.Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["Auth.Audience"],
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+        };
+
+        options.RefreshInterval = TimeSpan.FromMinutes(30);
+        options.RefreshOnIssuerKeyNotFound = true;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +44,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(options => options
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 
 app.UseAuthorization();
 
