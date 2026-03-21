b5ef3d984c4eb5b86ea7de88469bebe92f95387a
Aleksei Orlov <alexeyorlov65@gmail.com>
2026-03-22 01:22:17 +0300

    my profile ui

--- studhack-ui/src/app/features/profile/services/profile.service.ts
+++ studhack-ui/src/app/features/profile/services/profile.service.ts
@@ -2,9 +2,11 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import {
+  type EventFullDto,
   injectStudhackApiClient,
   type BootstrapDto,
   type MyProfileDto,
+  type TeamRequestsFeedDto,
   type UpsertMyProfileRequest,
   type UserFullDto,
 } from '@core/api';
@@ -19,8 +21,20 @@ export class ProfileService {
     return this.api.getBootstrap();
   }
 
+  getEvents(): Observable<readonly EventFullDto[]> {
+    return this.api.getEvents();
+  }
+
+  getUsers(): Observable<readonly UserFullDto[]> {
+    return this.api.getUsers();
+  }
+
   getUser(userId: string): Observable<UserFullDto> {
     return this.api.getUser(userId);
+  }
+
+  getTeamRequests(): Observable<TeamRequestsFeedDto> {
+    return this.api.getTeamRequests();
   }
 
   saveProfile(payload: UpsertMyProfileRequest): Observable<MyProfileDto> {
