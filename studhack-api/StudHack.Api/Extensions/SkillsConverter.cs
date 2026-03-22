cd00e01ed1c917d5fdf8a85711b2ef99e99651b3
SergeiKrivko <sergik.krivko@gmail.com>
2026-03-22 09:37:54 +0300

    add skill level

--- studhack-api/StudHack.Api/Extensions/SkillsConverter.cs
+++ studhack-api/StudHack.Api/Extensions/SkillsConverter.cs
@@ -1,5 +1,8 @@
 ﻿using StudHack.Domain.Models;
 using StudHack.Api.Dtos;
+using StudHack.Api.Enums;
+using StudHack.Domain;
+using StudHack.Domain.Enums;
 
 namespace StudHack.Api.Extensions;
 
@@ -17,5 +20,20 @@ public static class SkillsConverter
     public static Skill ToDomain(this SkillDto dto)
     {
         return new Skill(dto.Id, dto.Name);
+    }
+
+    public static UserSkillDto ToDto(this UserSkill skill)
+    {
+        return new UserSkillDto
+        {
+            Id = skill.Id,
+            Name = skill.Name,
+            Level = (SkillLevelDto)skill.Level,
+        };
+    }
+
+    public static UserSkill ToDomain(this UserSkillDto dto)
+    {
+        return new UserSkill(dto.Id, dto.Name, (ExperienceLevel)dto.Level);
     }
 }
---
+++ studhack-api/StudHack.Domain/Models/UserSkill.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using StudHack.Domain.Enums;
+
+namespace StudHack.Domain.Models;
+
+public class UserSkill : Skill
+{
+    public UserSkill(Guid id, string name, ExperienceLevel level) : base(id, name)
+    {
+        Level = level;
+    }
+
+    [Required] public ExperienceLevel Level { get; }
+}
