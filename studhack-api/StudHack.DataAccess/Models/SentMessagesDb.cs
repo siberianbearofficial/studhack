20fe60746eec0746bc9806032cc7bc411b3f3133
Kulikov Egor <kulikovegorandreevich@mail.ru>
2026-03-22 10:09:13 +0300

    final fix

--- studhack-api/StudHack.DataAccess/Models/SentMessagesDb.cs
+++ studhack-api/StudHack.DataAccess/Models/SentMessagesDb.cs
@@ -6,14 +6,16 @@ public class SentMessageDb
 {
     internal SentMessageDb() { }
 
-    public SentMessageDb(Guid idEventDate, Guid idSubscription)
+    public SentMessageDb(Guid idEventDate, Guid idSubscription, string message)
     {
         IdEventDate = idEventDate;
         IdSubscription = idSubscription;
+        Message = message;
     }
 
     public Guid IdEventDate { get; set; }
     public Guid IdSubscription { get; set; }
+    public string Message { get; set; }
     public virtual EventDateDb EventDate { get; set; }
     public virtual SubscriptionDb Subscription { get; set; }
 }
--- studhack-api/StudHack.DataAccess/Repositories/NotificationsRepository.cs
+++ studhack-api/StudHack.DataAccess/Repositories/NotificationsRepository.cs
@@ -12,53 +12,6 @@ public class NotificationsRepository(StudHackDbContext dbContext): INotification
 
     public async Task<List<MessageToSend>> GetMessagesToSend()
     {
-        var potentialMessages = await _dbContext.Subscriptions
-            .Include(s => s.User)
-            .Include(s => s.Event)
-                .ThenInclude(e => e.EventDates)
-            .Where(s => s.Event.EventDates.Any(ed =>
-                ed.StartsAt > DateTime.Now && // Только будущие даты
-                !_dbContext.SentMessages.Any(sm =>
-                    sm.IdSubscription == s.Id &&
-                    sm.IdEventDate == ed.Id))) // Не отправлено
-            .ToListAsync();
-
-        var messagesToSend = new List<MessageToSend>();
-
-        foreach (var subscription in potentialMessages)
-        {
-            foreach (var eventDate in subscription.Event.EventDates
-                .Where(ed => ed.StartsAt > DateTime.Now))
-            {
-                // Проверяем еще раз, не отправлено ли
-                var alreadySent = await _dbContext.SentMessages
-                    .AnyAsync(sm => sm.IdSubscription == subscription.Id &&
-                                   sm.IdEventDate == eventDate.Id);
-
-                if (!alreadySent)
-                {
-                    var message = string.Format("Привет, {0}!\n Напоминаем про " +
-                        "дедлайн по событию {1}: {2} в {3}", 
-                        subscription.User.UniqueName,
-                        subscription.Event.Title,
-                        eventDate.Description,
-                        eventDate.StartsAt);
-
-                    messagesToSend.Add(new MessageToSend(
-                        subscription.Id,
-                        eventDate.Id,
-                        message,
-                        subscription.User.Email
-                        ));
-                }
-            }
-        }
-
-        return messagesToSend;
-    }
-
-    public async Task<List<MessageToSend>> GetMessagesToSend2()
-    {
         // военные преступления
         var potentialMessages = await _dbContext.Subscriptions
             .Include(s => s.User)
@@ -111,7 +64,7 @@ public class NotificationsRepository(StudHackDbContext dbContext): INotification
     public async Task MarkSent(List<MessageToSend> messages)
     {
         foreach (var message in messages)
-            _dbContext.SentMessages.Add(new SentMessageDb(message.EventDateId, message.SubscriptionId));
+            _dbContext.SentMessages.Add(new SentMessageDb(message.EventDateId, message.SubscriptionId, message.Message));
         await _dbContext.SaveChangesAsync();
     }
 }
